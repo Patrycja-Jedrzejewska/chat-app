9b1b1c48331c932688d13c1a5cbe4f75f08106e7
Patrycja Jedrzejewska <patrycja.jedrzejewska.pl@gmail.com>
2023-05-16 13:54:33 +0200

    Create NoAccessView and NotFoundView content

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,1 +1,10 @@
-<template></template>
+<template>
+    <div class="card">
+      <header class="headline">404: Page not found</header>
+      <p>
+        Please go to
+        <router-link to="/login" class="link"> Login Form </router-link>
+      </p>
+    </div>
+</template>
+  
