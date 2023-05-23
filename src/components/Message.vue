2626c75455e38bac5f1f576e501a96c4772e246a
Patrycja Jedrzejewska <patrycja.jedrzejewska.pl@gmail.com>
2023-05-23 14:05:06 +0200

    Create realtime chat

---
+++ src/components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="message" >
+       <!-- <img class="message__avatar" :src="message.userPhotoURL" alt="`Avatar of ${message.userName}" />-->
+        <div class="message__author">{{ message.displayName }}</div>
+        <div class="message__text">{{ message.text }}</div>
+        <div class="message__date">{{ message.createdAt }}</div>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        message:{
+            type: Object,
+            required: true
+        }
+    },
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/NewMessage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="new-message">
+        <input type="text" class="new-message__input" v-model="newMessage" @keyup.enter="sendMessage" placeholder="Type a message...">
+        <button class="new-message__send-btn" @click="sendMessage">WYŚLIJ</button>
+    </div>
+</template>
+<script>
+import { ref } from 'vue'
+import { auth } from '../firebase/index'
+import { sendMessage } from '../chat/index'
+
+export default{
+    setup(){
+        const user=ref(auth.currentUser)
+        const newMessage=ref('')
+        return{
+            user,
+            newMessage,
+            sendMessage: ()=> sendMessage(user,newMessage)
+        }
+   }
+}
+</script>
